e76f29e90da40a34994c390a9785a87775dd808f
David Tran <davidt93@uw.edu>
2014-12-10 22:40:46 -0800

    fix angle copy problem

--- pyramid.js
+++ pyramid.js
@@ -1,16 +1,16 @@
 var x_angle = 0;
 var y_angle = 0;
-var z_angle = 0;
+var z_angle = 90;
 
 window.onload = function() {
 	var a = new Point3(50, -50, 0);
 	var b = new Point3(100, -50, 0);
 	var c = new Point3(50, 50, 0);
 	var d = new Point3(100, 50, 0);
-	var a = new Point3(50, -50, -50);
-	var b = new Point3(100, -50, -50);
-	var c = new Point3(50, 50, -50);
-	var d = new Point3(100, 50, -50);
+	var e = new Point3(50, -50, -50);
+	var f = new Point3(100, -50, -50);
+	var g = new Point3(50, 50, -50);
+	var h = new Point3(100, 50, -50);
 
 
 	var cc = document.getElementById("Canvas");
@@ -31,12 +31,64 @@ window.onload = function() {
 	ctx.lineTo(d.x_pos + 100,d.y_pos + 100);
 	ctx.stroke();
 
-
-	// PointRotation(TestPoint);
+	// ctx.moveTo(100,100);
+	// ctx.lineTo(e.x_pos + 100,e.y_pos + 100);
+	// ctx.stroke();
+	
+	// ctx.moveTo(100,100);
+	// ctx.lineTo(f.x_pos + 100,f.y_pos + 100);
+	// ctx.stroke();
 
 	// ctx.moveTo(100,100);
-	// ctx.lineTo(TestPoint.x_pos + 100,TestPoint.y_pos + 100);
+	// ctx.lineTo(g.x_pos + 100,g.y_pos + 100);
 	// ctx.stroke();
+
+	// ctx.moveTo(100,100);
+	// ctx.lineTo(h.x_pos + 100,h.y_pos + 100);
+	// ctx.stroke();
+
+	PointRotation(a);
+	PointRotation(b);
+	PointRotation(c);
+	PointRotation(d);
+	// PointRotation(e);
+	// PointRotation(f);
+	// PointRotation(g);
+	// PointRotation(h);
+
+
+	ctx.moveTo(100,100);
+	ctx.lineTo(a.x_pos + 100,a.y_pos + 100);
+	ctx.stroke();
+	
+	ctx.moveTo(100,100);
+	ctx.lineTo(b.x_pos + 100,b.y_pos + 100);
+	ctx.stroke();
+
+	ctx.moveTo(100,100);
+	ctx.lineTo(c.x_pos + 100,c.y_pos + 100);
+	ctx.stroke();
+
+	ctx.moveTo(100,100);
+	ctx.lineTo(d.x_pos + 100,d.y_pos + 100);
+	ctx.stroke();
+
+	// ctx.moveTo(100,100);
+	// ctx.lineTo(e.x_pos + 100,e.y_pos + 100);
+	// ctx.stroke();
+	
+	// ctx.moveTo(100,100);
+	// ctx.lineTo(f.x_pos + 100,f.y_pos + 100);
+	// ctx.stroke();
+
+	// ctx.moveTo(100,100);
+	// ctx.lineTo(g.x_pos + 100,g.y_pos + 100);
+	// ctx.stroke();
+
+	// ctx.moveTo(100,100);
+	// ctx.lineTo(h.x_pos + 100,h.y_pos + 100);
+	// ctx.stroke();
+
 
 
 };
@@ -55,17 +107,17 @@ function PointRotation(p) {
 	var z_copy = p.z_pos;
 
 	// convert degree to radians
-	x_angle = x_angle / 180.0 * Math.PI; 
-	y_angle = y_angle / 180.0 * Math.PI;
-	z_angle = z_angle / 180.0 * Math.PI;
+	var x_angle_copy = x_angle / 180.0 * Math.PI; 
+	var y_angle_copy = y_angle / 180.0 * Math.PI;
+	var z_angle_copy = z_angle / 180.0 * Math.PI;
 
 
-	var cx = Math.cos(x_angle);
-	var cy = Math.cos(y_angle);
-	var cz = Math.cos(z_angle);
-	var sx = Math.sin(x_angle);
-	var sy = Math.sin(y_angle);
-	var sz = Math.sin(z_angle);
+	var cx = Math.cos(x_angle_copy);
+	var cy = Math.cos(y_angle_copy);
+	var cz = Math.cos(z_angle_copy);
+	var sx = Math.sin(x_angle_copy);
+	var sy = Math.sin(y_angle_copy);
+	var sz = Math.sin(z_angle_copy);
 
 	var xx = cz*cx;
 	var xy = sz*cx;
