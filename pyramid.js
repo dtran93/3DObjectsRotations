54cf9f27881f2d1b8fb5057499a28980e70d1786
David Tran <davidt93@uw.edu>
2014-12-13 14:52:35 -0800

    it works need to clean up code

--- pyramid.js
+++ pyramid.js
@@ -5,64 +5,144 @@ var z_angle = 0;
 var cc; 
 var ctx;
 
-var LeftDiag = new Line(new Point3(75, 12.5, 0), new Point3(100, -30.8, 0));
-var RightDiag = new Line(new Point3(125, 12.5, 0), new Point3(100, -30.8, 0));
-var Bottom = new Line(new Point3(75, 12.5, 0), new Point3(125, 12.5, 0));
-var TopToCenter = new Line(new Point3(100, -30.8, 0), new Point3(100, 0, 43.3));
-var LeftToCenter = new Line(new Point3(75, 12.5, 0), new Point3(100, 0, 43.3));
-var RightToCenter = new Line(new Point3(125, 12.5, 0), new Point3(100, 0, 43.3));
+// var LeftDiag = new Line(new Point3(75, 12.5, 0), new Point3(100, -30.8, 0));
+// var RightDiag = new Line(new Point3(125, 12.5, 0), new Point3(100, -30.8, 0));
+// var Bottom = new Line(new Point3(75, 12.5, 0), new Point3(125, 12.5, 0));
+// var TopToCenter = new Line(new Point3(100, -30.8, 0), new Point3(100, 0, 43.3));
+// var LeftToCenter = new Line(new Point3(75, 12.5, 0), new Point3(100, 0, 43.3));
+// var RightToCenter = new Line(new Point3(125, 12.5, 0), new Point3(100, 0, 43.3));
 
-function myFunction() {
-    var x = document.getElementById("myForm").elements;
-    //document.getElementById("demo").innerHTML = x;
-    x_angle = parseInt(x[0].value);
-    y_angle = parseInt(x[1].value);
-    z_angle = parseInt(x[2].value);
-    //cc.clearRect(0, 0, canvas.width, canvas.height)
-    transforDrawing();
-    console.log(x_angle);
-    console.log(y_angle);
-    console.log(z_angle);
-    redraw();
+var linesPyramid;
+var intervalRotation = 100
+var timer;
+
+function Reset() {
+	ctx.beginPath();
+	ctx.clearRect ( 0 , 0 , cc.width, cc.height );
+	setLines();
+	redraw();
 }
+
+function mouseUp() {
+	clearInterval(timer);
+}
+
+function XangleP() {
+	timer = setInterval(function(){
+		x_angle = 5;
+	    y_angle = 0;
+	    z_angle = 0;
+	    erasePyramid();
+	    transforDrawing();
+	    redraw();
+	}, intervalRotation);
+}
+
+function XangleM() {
+	timer = setInterval(function(){
+	    x_angle = -5;
+	    y_angle = 0;
+	    z_angle = 0;
+	    erasePyramid();
+	    transforDrawing();
+	    redraw();
+    }, intervalRotation);
+}
+
+function YangleP() {
+	timer = setInterval(function(){
+	    x_angle = 0;
+	    y_angle = 5;
+	    z_angle = 0;
+	    erasePyramid();
+	    transforDrawing();
+	    redraw();
+    }, intervalRotation);
+}
+
+function YangleM() {
+	timer = setInterval(function(){
+	    x_angle = 0;
+	    y_angle = -5;
+	    z_angle = 0;
+	    erasePyramid();
+	    transforDrawing();
+	    redraw();
+    }, intervalRotation);
+}
+
+function ZangleP() {
+	timer = setInterval(function(){
+	    x_angle = 0;
+	    y_angle = 0;
+	    z_angle = 5;
+	    erasePyramid();
+	    transforDrawing();
+	    redraw();
+    }, intervalRotation);
+}
+
+function ZangleM() {
+	timer = setInterval(function(){
+	    x_angle = 0;
+	    y_angle = 0;
+	    z_angle = -5;
+	    erasePyramid();
+	    transforDrawing();
+	    redraw();
+    }, intervalRotation);
+}
+
 
 window.onload = function() {
 	cc = document.getElementById("Canvas");
 	ctx = cc.getContext("2d");
-	DrawOrigin();
-	DrawLine(LeftDiag, "#000000");
-	DrawLine(RightDiag, "#000000")
-	DrawLine(Bottom, "#000000");
-	DrawLine(TopToCenter, "#000000");
-	DrawLine(LeftToCenter, "#000000");
-	DrawLine(RightToCenter, "#000000");
+	setLines();
+	redraw();
 };
 
+function setLines() {
+	var LeftDiag = new Line(new Point3(-50, 25, 0), new Point3(0, -61.6, 0));
+	var RightDiag = new Line(new Point3(50, 25, 0), new Point3(0, -61.6, 0));
+	var Bottom = new Line(new Point3(-50, 25, 0), new Point3(50, 25, 0));
+	var TopToCenter = new Line(new Point3(0, -61.6, 0), new Point3(0, 0, 86.6));
+	var LeftToCenter = new Line(new Point3(-50, 25, 0), new Point3(0, 0, 86.6));
+	var RightToCenter = new Line(new Point3(50, 25, 0), new Point3(0, 0, 86.6));
+	linesPyramid = [LeftDiag, RightDiag, Bottom, TopToCenter, LeftToCenter, RightToCenter];
+}
 
 function redraw() {
-	DrawLine(LeftDiag, "#000000");
-	DrawLine(RightDiag, "#000000")
-	DrawLine(Bottom, "#000000");
-	DrawLine(TopToCenter, "#000000");
-	DrawLine(LeftToCenter, "#000000");
-	DrawLine(RightToCenter, "#000000");
+	DrawOrigin();
+	ctx.beginPath();
+	for (var i = 0; i < linesPyramid.length; i++) {
+		DrawLine(linesPyramid[i]);
+	}
 }
 
 function transforDrawing() {
-	LineRotation(LeftDiag);
-	LineRotation(RightDiag);
-	LineRotation(Bottom);
-	LineRotation(TopToCenter);
-	LineRotation(LeftToCenter);
-	LineRotation(RightToCenter);
+	for (var i = 0; i < linesPyramid.length; i++) {
+		LineRotation(linesPyramid[i]);
+	}
 }
 
-function DrawLine(line, color) {
-	if (color == "#000000") {
-		ctx.strokeStyle="#000000";
-	} else {
-		ctx.strokeStyle="#FFFFFF";
+function erasePyramid() {
+	for (var i = 0; i < linesPyramid.length; i++) {
+		ctx.beginPath();
+		ctx.strokeStyle="#ffffff";
+		ctx.moveTo(linesPyramid[i].p1.x_pos + 150,linesPyramid[i].p1.y_pos + 150);
+		ctx.lineTo(linesPyramid[i].p2.x_pos + 150,linesPyramid[i].p2.y_pos + 150);
+		ctx.stroke();
+		ctx.moveTo(linesPyramid[i].p1.x_pos + 150,linesPyramid[i].p1.y_pos + 150);
+		ctx.lineTo(linesPyramid[i].p2.x_pos + 150,linesPyramid[i].p2.y_pos + 150);
+		ctx.stroke();
+		ctx.moveTo(linesPyramid[i].p1.x_pos + 150,linesPyramid[i].p1.y_pos + 150);
+		ctx.lineTo(linesPyramid[i].p2.x_pos + 150,linesPyramid[i].p2.y_pos + 150);
+		ctx.stroke();
 	}
+}
+
+function DrawLine(line) {
+	ctx.strokeStyle="#ff0000";
 	ctx.moveTo(line.p1.x_pos + 150,line.p1.y_pos + 150);
 	ctx.lineTo(line.p2.x_pos + 150,line.p2.y_pos + 150);
 	ctx.stroke();
@@ -74,6 +154,10 @@ function Line (p1, p2) {
 }
 
 function DrawOrigin() {
+	// bug need to chop of line or it will change all the lines' colors
+	ctx.beginPath();
+	ctx.strokeStyle="#000000";
+
 	ctx.moveTo(0,150);
 	ctx.lineTo(300,150);
 	ctx.stroke();
@@ -93,10 +177,8 @@ function LineRotation(line) {
 	PointRotation(line.p1);
 	PointRotation(line.p2);
 }
+
 function PointRotation(p) {
-	// console.log(p.x_pos);
-	// console.log(p.y_pos);
-	// console.log(p.z_pos);
 	var x_copy = p.x_pos;
 	var y_copy = p.y_pos;
 	var z_copy = p.z_pos;
@@ -130,8 +212,4 @@ function PointRotation(p) {
 	p.x_pos = x_copy * xx + y_copy * xy + z_copy * xz;
 	p.y_pos = x_copy * yx + y_copy * yy + z_copy * yz;
 	p.z_pos = x_copy * zx + y_copy * zy + z_copy * zz;
-
-	// console.log(p.x_pos);
-	// console.log(p.y_pos);
-	// console.log(p.z_pos);
 }
